8ac606f2e21a20298ac55dbfb01dd0be75476b46
Chandramani Kumar <kchandramani8@gmail.com>
2026-02-11 21:11:05 +0530

    Swap geofence values for keypress events

--- scripts/z-mx.js
+++ scripts/z-mx.js
@@ -1,20 +1,25 @@
+
+// Z MX - Geofence Setter
+// Press A to set Geofence to 25
+// Press Z to set Geofence to 50
+
 (function() {
     'use strict';
 
     document.addEventListener('keydown', (event) => {
-        if (event.key === 'z' || event.key === 'Z') {
-            const textbox = document.querySelector('#geofence');
-            if (textbox) {
-                textbox.select();
-                document.execCommand('insertText', false, '50');
-            }
-        }
-
         if (event.key === 'a' || event.key === 'A') {
             const textbox = document.querySelector('#geofence');
             if (textbox) {
                 textbox.select();
                 document.execCommand('insertText', false, '25');
+            }
+        }
+
+        if (event.key === 'z' || event.key === 'Z') {
+            const textbox = document.querySelector('#geofence');
+            if (textbox) {
+                textbox.select();
+                document.execCommand('insertText', false, '50');
             }
         }
     });
