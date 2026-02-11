cb52be276c285fae1f2772dd9e8b9e0c7a53c677
Chandramani Kumar <kchandramani8@gmail.com>
2026-02-11 20:37:09 +0530

    Refactor US remarks user script for clarity and updates

--- scripts/remarks-us.js
+++ scripts/remarks-us.js
@@ -1,15 +1,8 @@
-// ==UserScript==
-// @name         GS 2.0 Remarks for US
-// @author       @manichk
-// @namespace    http://tampermonkey.net/
-// @version      1.1
-// @description  US emarks for different scenarios
-// @match        https://eu.templates.geostudio.last-mile.amazon.dev/*
-// @match        https://na.templates.geostudio.last-mile.amazon.dev/*
-// @match        https://fe.templates.geostudio.last-mile.amazon.dev/*
-// ==/UserScript==
+// GS 2.0 Remarks for US
+
 (function () {
     'use strict';
+
     const selectList = document.createElement('select');
     selectList.style.position = 'absolute';
     selectList.style.left = '40%';
@@ -25,10 +18,12 @@
     selectList.style.outline = 'none';
     selectList.style.zIndex = '9999';
     selectList.style.width = '150px';
+
     const defaultOption = document.createElement('option');
     defaultOption.value = -1;
     defaultOption.text = 'US Remarks';
     selectList.appendChild(defaultOption);
+
     const options = [
         'Delivery Hints',
         'Preferred UPID Delivery',
@@ -37,72 +32,54 @@
         'RE is disabled',
         'Leasing / Locker not Found',
         'Mailroom not Found',
-        //'Locality Mismatch',
-        //'Zip Issue',
-        //'City_Country Mismatch',
-        //'long road',
-        //'Zip is on the same road.',
-        //'CX_Hint Address Coflict',
-       // 'NAK Issue',
-        //'Within Locality',
-        //'Multiple Road',
-        //'Partially Matching Road',
-        //'Adjacent locality',
-        //'Long Road / No GCRS',
-        //'Combination Not Found',
         'Traffic Road issue',
-        'NotFoundin3P',
-       // 'Direct Search'
+        'NotFoundin3P'
     ];
+
     const textToInsert = {
         'Delivery Hints': 'Geocorrected based on delivery hints provided by the customer',
         'Preferred UPID Delivery': 'Geocorrected based on the UPID Delivery',
         'RE and DP is disabled': 'RE and DP is disabled.',
         'DP is disabled': 'DP is disabled.',
         'RE is disabled': 'RE is disabled',
-        'Leasing not Found': 'Leasing office or Locker not Found in any of the 3P and site map',
+        'Leasing / Locker not Found': 'Leasing office or Locker not Found in any of the 3P and site map',
         'Mailroom not Found': 'Mailroom not Found in any of the 3P and site map',
-        'Zip is on the same road.': 'Zip is on the same road.',
-        'Zip Issue': 'ZIP is Not Matching.',
-        'City_Country Mismatch': 'City , Country Not Matching',
-        'CX_Hint Address Coflict':'CX address and delivery hint address are conflicting',
-        'NAK Issue': 'Different Road or Locality or building number Learned',
-        'Locality Mismatch':'Locality is not Matching  or Not locatable or too far.',
-        'Within Locality':'Marked within locality , using bing right click validation wrtx Locality bouandry.',
-        'Multiple Road':'Multiple roads with same name or Multiple Prefix, Suffix',
-        'Partially Matching Road':'Partially Matching street',
-        'Adjacent locality':'Adjacent locality Found',
-        'Long Road / No GCRS':'long road and no GCRS and bing is also not taking to the building or nearby building number ',
-        'Combination Not Found':'Not found the road combination mentioned By CX',
         'Traffic Road issue': 'Traffic roads  service roads not enabling  GS issue.',
-        'Direct Search':'Marked till road level based on bing direct search + nearby number'
+        'NotFoundin3P': 'Not able to locate in 3P'
     };
+
     options.forEach((optionText, index) => {
         const option = document.createElement('option');
         option.value = index;
         option.text = optionText;
         selectList.appendChild(option);
     });
+
     let isPressed = false;
     let isTextboxFocused = false;
+
     selectList.addEventListener('mousedown', () => {
         isPressed = true;
         selectList.style.backgroundColor = '#d3e5ff';
     });
+
     selectList.addEventListener('mouseup', () => {
         isPressed = false;
         selectList.style.backgroundColor = '#fff';
     });
+
     selectList.addEventListener('mouseout', () => {
         if (!isPressed && !isTextboxFocused) {
             selectList.style.backgroundColor = '#fff';
         }
     });
+
     selectList.addEventListener('mouseenter', () => {
         if (!isPressed) {
             selectList.style.backgroundColor = '#f2f2f2';
         }
     });
+
     selectList.addEventListener('change', () => {
         const selectedIndex = selectList.value;
         if (selectedIndex >= 0) {
@@ -142,15 +119,18 @@
             }
         }
     });
+
     selectList.addEventListener('focus', () => {
         selectList.classList.add('open');
     });
+
     selectList.addEventListener('blur', () => {
         selectList.classList.remove('open');
         if (!isTextboxFocused) {
-            selectList.style.width = '110px';
+            selectList.style.width = '150px';
         }
     });
+
     selectList.addEventListener('keydown', (event) => {
         if (event.key === 'ArrowUp' || event.key === 'ArrowDown') {
             const selectedIndex = selectList.selectedIndex;
@@ -162,17 +142,19 @@
             event.preventDefault();
         }
     });
+
     document.body.appendChild(selectList);
+
     const textbox = document.querySelector("#remarks");
     if (textbox) {
         textbox.addEventListener('focus', () => {
             isTextboxFocused = true;
-            selectList.style.width = '110px';
+            selectList.style.width = '150px';
         });
         textbox.addEventListener('blur', () => {
             isTextboxFocused = false;
             if (!selectList.classList.contains('open')) {
-                selectList.style.width = '110px';
+                selectList.style.width = '150px';
             }
         });
     }
