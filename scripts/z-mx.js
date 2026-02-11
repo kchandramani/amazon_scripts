733c439356a25fc5093bd05e7f19787970f28eec
Chandramani Kumar <kchandramani8@gmail.com>
2026-02-11 21:05:34 +0530

    Add functionality to insert '25' on 'A' key press

--- scripts/z-mx.js
+++ scripts/z-mx.js
@@ -9,5 +9,13 @@
                 document.execCommand('insertText', false, '50');
             }
         }
+
+        if (event.key === 'a' || event.key === 'A') {
+            const textbox = document.querySelector('#geofence');
+            if (textbox) {
+                textbox.select();
+                document.execCommand('insertText', false, '25');
+            }
+        }
     });
 })();
